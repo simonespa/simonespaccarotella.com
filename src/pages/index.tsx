fb804e44822091bacdddbdef5928f07476b04e51
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2021-10-07 14:22:58 +0100

    Add PR template, update config and home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,26 @@ export default function Home() {
         <title>Spaccarotella&apos;s Blog</title>
       </Head>
 
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-        <div className="flex-shrink-0">
-          <Image className="h-12 w-12" src="/user.jpeg" width="60" height="60" alt="User Logo" />
-        </div>
-        <div>
-          <div className="text-xl font-medium text-black">Hi</div>
-          <p className="text-gray-500">Welcome to my personal website</p>
-        </div>
-      </div>
+      <header>
+        <nav></nav>
+      </header>
+
+      <main>
+        <section>
+          <article>
+            <header>
+              <h2>The Planet Earth</h2>
+              <p>Posted on Wednesday, <time dateTime="2017-10-04">4 October 2017</time> by Jane Smith</p>
+            </header>
+            <p>We live on a planet that is blue and green, with so many things still unseen.</p>
+            <p><a href="https://example.com/the-planet-earth/">Continue reading....</a></p>
+          </article>
+        </section>
+        <section></section>
+        <section></section>
+      </main>
+
+      <footer></footer>
     </>
   );
 }
