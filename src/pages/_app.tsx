41fa18c55f1edfef85745d94d377ebd346098b77
Simone Spaccarotella <simone.spaccarotella@bbc.co.uk>
2023-01-15 21:26:47 +0000

    Add Google Analytics tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // https://nextjs.org/docs/messages/next-script-for-ga
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-HSRHBVMXN0"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy='afterInteractive'>
+        {`
+          <!-- Google tag (gtag.js) -->
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-HSRHBVMXN0');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
